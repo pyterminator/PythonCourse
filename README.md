b1ff45b1f7f0ddca96c9f175b988cc53419c4a77
MushvigShukurov <musfiqsukurov9@gmail.com>
2024-06-10 02:09:03 +0400

    Dərs-3,4,5

--- README.md
+++ README.md
@@ -31,3 +31,33 @@ Ekrana Salam dünya! (Hello world!) yazdırmaq üçün <b>__hello__</b> modulund
     <li>complex ədədin real parametri və imag (Yəni imaginary) parametrləri</li>
 </ul> 
 <hr> 
+
+## Dərs 3 - də öyrənəcəkləriniz 
+VSCode yükləmək üçün <a href="https://code.visualstudio.com/download">link</a>
+VSCode-da select default profile = cmd
+AutoSave
+OpenFolder
+Zoom in\out
+Extensions
+Python programı run et (çalışdır)
+CTRL + K və CTRL + ` ilə terminalı aç
+<hr>
+
+## Dərs 4 - də öyrənəcəkləriniz 
+Şərh sətirləri - Tək sətirli, Çox sətirli
+İnteger literal (Binary, Hexadecimal, Octal və onların imzası 0b və ya 0B / 0x və ya 0X / 0o və ya 0O)
+int() funksiyası, conversion bin|hex|oct to decimal, base parametri
+String literal
+İndeks məntiqi
+print() funksiyası içərisində 1-dən çox parametr daxil etmək, sep parametri (new line escape)
+String reverse
+String slicing
+String concat
+f string (+format string)
+Çox sətirli və tək sətirli stringlər
+Single & double quotes in string
+<hr>
+
+## Dərs 5 - də öyrənəcəkləriniz 
+İnput funksiyası, mesajın verilməsi, istifadəçidən məlumatın alınması
+<hr> 
--- pyterminator.ders-notları.py
+++ pyterminator.ders-notları.py
@@ -30,3 +30,36 @@
     complex() funksiyası | Qeyd : bu bir built-in function-dır. Yəni hazır funksiyadır.
     complex ədədin real parametri və imag (Yəni imaginary) parametrləri 
 """
+
+# Dərs 3 - də öyrənəcəkləriniz 
+"""
+    VSCode yükləmək üçün <a href="https://code.visualstudio.com/download">link</a>
+    VSCode-da select default profile = cmd
+    AutoSave
+    OpenFolder
+    Zoom in\out
+    Extensions
+    Python programı run et (çalışdır)
+    CTRL + K və CTRL + ` ilə terminalı aç 
+"""
+
+# Dərs 4 - də öyrənəcəkləriniz 
+"""
+    Şərh sətirləri - Tək sətirli, Çox sətirli
+    İnteger literal (Binary, Hexadecimal, Octal və onların imzası 0b və ya 0B / 0x və ya 0X / 0o və ya 0O)
+    int() funksiyası, conversion bin|hex|oct to decimal, base parametri
+    String literal
+    İndeks məntiqi
+    print() funksiyası içərisində 1-dən çox parametr daxil etmək, sep parametri (new line escape)
+    String reverse
+    String slicing
+    String concat
+    f string (+format string)
+    Çox sətirli və tək sətirli stringlər
+    Single & double quotes in string 
+"""
+
+# Dərs 5 - də öyrənəcəkləriniz 
+"""
+    İnput funksiyası, mesajın verilməsi, istifadəçidən məlumatın alınması
+""" 
