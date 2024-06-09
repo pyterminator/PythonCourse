1017dddb2e5b4193bc2c34d3d1f4ada9c4dff841
MushvigShukurov <musfiqsukurov9@gmail.com>
2024-06-09 07:51:56 +0400

    Dərs-2

--- pyterminator.ders-notları.py
+++ pyterminator.ders-notları.py
@@ -1,4 +1,4 @@
-# Dərs 1 - in açıqlaması
+# Dərs 1 - də öyrənəcəkləriniz
 """ 
     Python yükləmək üçün link https://www.python.org/downloads/ 
     Mən 3.11.9 versiyasını yüklədim.
@@ -8,4 +8,25 @@
     __helo__ modulunu import et
 
     print funksiyasını tanı
+"""
+
+# Dərs 2 - də öyrənəcəkləriniz
+"""
+    type() function | Qeyd : bu bir built-in function-dır. Yəni hazır funksiyadır.
+    
+    Python data type-lardan NUMERİC type-lar
+        INTeger literal nədir ?
+        Float literal nədir ?
+        Complex literal nədir ? (Riyaziyyatdakı kompleks ədədlər\Həqiqi və Xəyali hissəsi)
+    
+    dir() function | Qeyd : bu bir built-in function-dır. Yəni hazır funksiyadır.
+
+    Riyazi əməllər (Topla, çıx, vur, böl).
+
+    int() funksiyası | Qeyd : bu bir built-in function-dır. Yəni hazır funksiyadır.
+    
+    float() funksiyası | Qeyd : bu bir built-in function-dır. Yəni hazır funksiyadır.
+    
+    complex() funksiyası | Qeyd : bu bir built-in function-dır. Yəni hazır funksiyadır.
+    complex ədədin real parametri və imag (Yəni imaginary) parametrləri 
 """
