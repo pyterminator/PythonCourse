94aac573a3af16e08a91a1b1c05f47956789f7ef
MushvigShukurov <musfiqsukurov9@gmail.com>
2024-08-10 13:57:41 +0400

    TASK-62

--- tasks.py
+++ tasks.py
@@ -104,4 +104,10 @@ Bu list içərisindəki minimum ədədi tapan funksiya yazın.
 
 TASK 61
 Ədədin bölənlərinin qaytaran funksiya yazın
+
+# TASK 62
+my_list = [9, 0, 2, 4, 5, 6, 8, 7, 3, 1]
+#          0  1  2  3  4  5  6  7  8  9
+Bu listdeki 2ci indeks ve 8-ci indeksde dayanan elementlerin yerini deyis.
+List bu sekilde olsun : [9, 0, 3, 4, 5, 6, 8, 7, 2, 1]
 """
