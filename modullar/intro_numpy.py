c44b66a21b93a285fbcb202a5c91325020ff510d
Mushvig Shukurov <musfiqsukurov9@gmail.com>
2024-09-06 23:57:59 +0400

    Introduction2Numpy

---
+++ modullar/intro_numpy.py
@@ -0,0 +1,41 @@
+import numpy
+
+A = numpy.array([[2,3,4],
+                 [1,0,9],
+                 [3,4,1]])
+B = numpy.array([[1,0,5],
+                 [2,4,1],
+                 [7,2,3]])
+
+# print(A @ B)
+# C = A.dot(B)
+# print(C)
+
+# print(A+B)
+# print(A - B)
+# print(A * B)
+
+
+# print(A)
+# print("-----------")
+# print(A.transpose())
+
+# Matrisin transponirəsi
+"""
+    2 3 4
+A = 1 0 9      3 x 3
+    3 4 1
+
+     2 1 3
+AT = 3 0 4
+     4 9 1
+
+    1 0 5
+B = 2 4 1
+    7 2 3
+
+         3  3  9
+A + B =  3  4 10
+        10  6  4
+"""
+
